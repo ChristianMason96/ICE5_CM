bf6e34f78a6fae0a5950765e61b2d2c70f1bff2c
ChristianMason96 <masochri@sheridancollege.ca>
2024-11-26 19:36:56 -0500

    another one

--- src/main/java/com/example/contactmanager/ice5/ICE5.java
+++ src/main/java/com/example/contactmanager/ice5/ICE5.java
@@ -9,7 +9,7 @@ package com.example.contactmanager.ice5;
  * @author Chris
  */
 public class ICE5 {
-
+    private int myProp;
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
