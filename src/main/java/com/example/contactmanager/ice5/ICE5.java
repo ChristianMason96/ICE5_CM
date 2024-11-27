8a208a37ef2b95ec65d2e11fbe090ce72018956d
ChristianMason96 <masochri@sheridancollege.ca>
2024-11-26 19:16:40 -0500

    "Initial Commit for StudentList Project"

---
+++ src/main/java/com/example/contactmanager/ice5/ICE5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.contactmanager.ice5;
+
+/**
+ *
+ * @author Chris
+ */
+public class ICE5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
